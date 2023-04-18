2b9975d05b50a3ceed2f33483c071ccd002bf034
Aidisonn <edisonlow0909@gmail.com>
2023-04-18 17:28:55 +0800

    created package

---
+++ app/src/main/java/my/edu/tarumt/ecolution/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package my.edu.tarumt.ecolution.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import my.edu.tarumt.ecolution.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
