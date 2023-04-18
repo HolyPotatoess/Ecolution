625fec580c80a5585b3eb4654b7774a4af1bdb9c
Aidisonn <edisonlow0909@gmail.com>
2023-04-18 17:12:14 +0800

    COM.EXAMPLE

--- app/src/main/java/my/edu/tarumt/ecolution/MainActivity.kt
+++ app/src/main/java/my/edu/tarumt/ecolution/MainActivity.kt
@@ -2,6 +2,8 @@ package my.edu.tarumt.ecolution
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ecolutionassignment.R
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
