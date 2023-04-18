9f5b3d8ab0180b6c32c30f330e10ca043914d8f5
SIEW WEN BIN <vegetasiew000@gmail.com>
2023-04-18 17:16:09 +0800

    nothing

--- app/src/main/java/my/edu/tarumt/ecolution/MainActivity.kt
+++ app/src/main/java/my/edu/tarumt/ecolution/MainActivity.kt
@@ -2,8 +2,6 @@ package my.edu.tarumt.ecolution
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.ecolutionassignment.R
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
